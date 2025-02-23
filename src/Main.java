66f0cf3c2048fb47cee0cf45d51f6646b587d8cd
BenedictusNelson <benedictus.nelson@gmail.com>
2025-02-23 21:30:09 +0700

    refactor: input from txt file

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,45 @@
-// import javax.swing.JFileChooser;
+import javax.swing.JFileChooser;
 // import java.io.File;
-// import java.io.FileNotFoundException;
 import java.util.Scanner;
 // import java.util.List;
 // import java.util.ArrayList;
-//import java.util.Arrays;
+// import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("masukkan N, M, P: ");
-        int N = sc.nextInt();
-        int M = sc.nextInt();  
-        int P = sc.nextInt();
-        sc.nextLine();
-        System.out.println("masukkan board: ");
-        // String mode = sc.nextLine().trim();
+            JFileChooser fileChooser = new JFileChooser();
+            int returnVal = fileChooser.showOpenDialog(null);
+            if (returnVal != JFileChooser.APPROVE_OPTION) {
+                System.out.println("Tidak ada file yang dipilih");
+                return;
+            }
 
-        PuzzlePiece[] pieces = new PuzzlePiece[P];
-        for (int i = 0; i < P; i++) {
-            // String data = sc.nextLine();
-            pieces[i] = new PuzzlePiece((char)('A' + i));
+            // File chosenFile = fileChooser.getSelectedFile();
+            Scanner sc = new Scanner(System.in);
+            try {
+                System.out.println("masukkan N, M, P: ");
+                int N = sc.nextInt();
+                int M = sc.nextInt();  
+                int P = sc.nextInt();
+                sc.nextLine();
+                System.out.println("masukkan board: ");
+                // String mode = sc.nextLine().trim();
+
+                PuzzlePiece[] pieces = new PuzzlePiece[P];
+                for (int i = 0; i < P; i++) {
+                    // String data = sc.nextLine();
+                    pieces[i] = new PuzzlePiece((char)('A' + i));
+                }
+                PuzzleSolver solver = new PuzzleSolver(N, M, pieces);
+                if (solver.solve()) {
+                    solver.printBoard();
+                } else {
+                    System.out.println("No solution found!");
+                }
+
+            } finally {
+                sc.close();
+            }
         }
-        PuzzleSolver solver = new PuzzleSolver(N, M, pieces);
-        if (solver.solve())
-            solver.printBoard();
-        else
-            System.out.println("No solution found!");
-        
-        sc.close();
     }
-}
+
